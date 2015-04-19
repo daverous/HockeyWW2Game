1c881be0e1626027fcc078c680ca37bf8a38bfdf
codymarcus <codydavidmarcus@gmail.com>
2015-04-19 14:08:21 -0400

    Equipable Controller

---
+++ WW2 Hockey Game/Assets/EquipableController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class EquipableController : MonoBehaviour {
+
+	public Equipable[] equipables;
+	Equipable equipped;
+
+	// Use this for initialization
+	void Start () {
+		equipped = equipables[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// If an equip button is pressed, set equipped to the appropriate equipable
+		for (int i=0;i<4;i++)
+			if (CrossPlatformInputManager.GetButtonDown("Equip" + i))
+			    equipped = equipables[i];
+	}
+}
